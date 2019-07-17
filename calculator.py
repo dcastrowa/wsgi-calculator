7350cc804dd5c9ea1f689b0216baf5c681b89981
Daniel Castro <daniel.castro@critigen.com>
2019-07-16 19:58:20 -0700

    Complete assignment04

--- calculator.py
+++ calculator.py
@@ -40,45 +40,102 @@ To submit your homework:
 
 
 """
+import traceback
+
+
+def index_page():
+    body = ['<h1>Here is how to use this page...</h1>',
+            '<ul>',
+            'add/3/5 ==> sum of 3+5</ul>',
+            '<ul>subtract/10/2 ==> result of 10-2</ul>',
+            '<ul>multiply2/10 ==> result of 2x10</ul>',
+            '<ul>divide/20/5 ==> result of 20/2</ul>']
+    return '\n'.join(body)
 
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
-
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+    sum = args[0] + args[1]
 
     return sum
 
-# TODO: Add functions for handling more arithmetic operations.
+
+def subtract(*args):
+    """Returns a STRING with the difference of the arguments"""
+    diff = int(args[0]) - int(args[1])
+
+    return str(diff)
+
+
+def multiply(*args):
+    """Returns a STRING with the multiplication of the arguments"""
+    result = int(args[0]) * int(args[1])
+
+    return str(result)
+
+
+def divide(*args):
+    """
+    Returns a STRING with the division of the arguments
+    or returns ZeroDivisionError
+    """
+    try:
+        result = int(args[0]) / int(args[1])
+        return str(result)
+
+    except ZeroDivisionError:
+        return 'Error! Divide by zero'
+
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
+    funcs = {
+        '': index_page,
+        'add': add,
+        'subtract': subtract,
+        'multiply': multiply,
+        'divide': divide
+    }
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
 
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
